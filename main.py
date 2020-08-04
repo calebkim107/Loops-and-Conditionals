940df15f7f413777208537a26ca1418265cf7841
calebkim107 <calebkim1117@gmail.com>
2020-08-04 19:41:24 +0000

    added loops and conditionals excersizes

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print
+print("Lopps and Conditionals exercise")
+
+# For loop
+# x is a loop variable
+for x in range(5):
+  print(x)
+
+for x in range(3, 6):
+  print(x)
+
+
+# While loop
+count = 0
+while (count < 3):
+  count += 1
+  print("Hello friend!")
+  print(count)
+  count +=1
+# CONDITIONALS
+
+i = 15
+if(i > 15):
+  print("i is more than 15.")
+  print("i'm in an if block")
+elif (i == 15):
+   print(" is equal to 15")
+   print("i'm in an else")
+else:
+  print("i is less than 15")
+  print("i'm in an else block")
